ce0c0ca028aff5696ae3df17f98fe2a5076cc8b1
Aloct <116685533+Aloct@users.noreply.github.com>
2024-12-23 21:49:05 +0100

    added mySQLWrapper, updated redisWrapper

--- dataHandling/dataOperations/dataOperations.go
+++
@@ -1,9 +0,0 @@
-package dataHandlingUtils
-
-func getData() {
-
-}
-
-func seData() {
-	
-}
--- dataHandling/storageWrapper/wrapperUtils.go
+++ dataHandling/storageWrapper/wrapperUtils.go
@@ -2,6 +2,7 @@ package wrapperUtils
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
@@ -15,6 +16,8 @@ type DBWrapper interface {
 	Query(ctx context.Context, query string) (interface{}, error)
 	RateLimit(ctx context.Context) error
 	rotatePW(ctx context.Context) error
+	getData()
+	setData()
 }
 
 type BaseWrapper struct {
@@ -44,10 +47,11 @@ func (b *BaseWrapper) PwRotation(ctx context.Context) error {
 
 type RedisWrapper struct {
 	BaseWrapper
+	DbNum int
 	DB *redis.Client
 }
 
-func NewRedisWrapper(rateLimit bool, pwRotation bool, subject string, container string, port int, password string) *RedisWrapper {
+func NewRedisWrapper(rateLimit bool, pwRotation bool, subject string, container string, port int, dbnum int, password string) *RedisWrapper {
 	redisClient := RedisWrapper{
 		BaseWrapper: BaseWrapper{
 			RateLimitEnabled: rateLimit,
@@ -57,6 +61,7 @@ func NewRedisWrapper(rateLimit bool, pwRotation bool, subject string, container 
 			Port: port,
 			Password: password,
 		},
+		DbNum: dbnum,
 	}
 
 	return &redisClient
@@ -66,7 +71,7 @@ func (r *RedisWrapper) Connect(ctx context.Context) error {
 	rdb := redis.NewClient(&redis.Options{
 		Addr: fmt.Sprintf("%s:%d", r.Container, r.Port), 
 		Password: r.Password, 
-		DB: 0,
+		DB: r.DbNum,
 	})
 
 	_, err := rdb.Ping(context.Background()).Result()
@@ -80,10 +85,54 @@ func (r *RedisWrapper) Connect(ctx context.Context) error {
 	return nil
 }
 
+func (r *RedisWrapper) getData() {
+
+}
+
+func (r *RedisWrapper) setData() {
+
+}
+
+
 type MongoWrapper struct {
 	BaseWrapper
 }
 
+
 type MySQLWrapper struct {
 	BaseWrapper
+	DB *sql.DB
+	DBname string
+	User string
+}
+
+func NewSQLWrapper(rateLimit bool, pwRotation bool, subject string, container string, port int, dbname string, user string, password string) *MySQLWrapper {
+	sqlWrapper := MySQLWrapper{
+		BaseWrapper: BaseWrapper{
+			RateLimitEnabled: rateLimit,
+			PwRotationEnabled: pwRotation,
+			Subject: subject,
+			Container: container,
+			Port: port,
+			Password: password,
+		},
+		DBname: dbname,
+		User: user,
+	}
+
+	return &sqlWrapper
+}
+
+func (sr *MySQLWrapper) Connect() {
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", sr.User, sr.Password, sr.Container, sr.Port, sr.DBname))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sr.DB = db
 }
