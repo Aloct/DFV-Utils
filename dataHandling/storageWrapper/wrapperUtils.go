495955b214d5c4fc51239d0a211f8015be7eeb9d
Aloct <aloct.itw@gmail.com>
2025-02-02 15:07:00 +0100

    fixed wrapperUtils

--- dataHandling/storageWrapper/wrapperUtils.go
+++ dataHandling/storageWrapper/wrapperUtils.go
@@ -3,27 +3,33 @@ package wrapperUtils
 import (
 	"context"
 	"database/sql"
+	"encoding/base64"
+	"errors"
 	"fmt"
 	"log"
 	"os"
+	"reflect"
 	"strconv"
+	"time"
 
+	"github.com/awnumar/memguard"
 	"github.com/go-redis/redis/v8"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
 )
 
 type DBWrapper interface {
-	// Connect(ctx context.Context) error
-	// Close() error
-	// // RateLimit(ctx context.Context) error
-	// // rotatePW(ctx context.Context) error
-	// getData(query string) map[int]map[string]any 
-	// setData(query string) 
+	Connect(ctx context.Context) error
+	// RateLimit(ctx context.Context) error
+	// rotatePW(ctx context.Context) error
+	GetData(query string,  values []any) (any, error)
+	SetData(query string, values []any, duration *time.Duration) error
+	GetKey(id string) (any, error)
+	SetKey(id string, key any, d *time.Duration) error
 }
 
 type DBPool struct {
-	pool map[string]DBWrapper
+	Pool map[string]DBWrapper
 }
 
 type BaseWrapper struct {
@@ -59,13 +65,18 @@ type RedisWrapper struct {
 
 func NewDBPool() *DBPool {
 	return &DBPool{
-		pool: make(map[string]DBWrapper, 0),
+		Pool: make(map[string]DBWrapper, 0),
 	}
 }
 
 func (p *DBPool) NewRedisWrapper(dbName string) (*RedisWrapper, error) {
-	if p.pool[dbName] != nil {
-		return p.pool[dbName].(*RedisWrapper), nil
+	if p.Pool[dbName] != nil {
+		err := p.Pool[dbName].Connect(context.Background())
+		if err != nil {
+			return nil, err
+		}
+
+		return p.Pool[dbName].(*RedisWrapper), nil
 	}
 
 	err := godotenv.Load(".env")
@@ -73,12 +84,17 @@ func (p *DBPool) NewRedisWrapper(dbName string) (*RedisWrapper, error) {
 		return nil, err
 	}
 
+	fmt.Println(dbName)
 	dbNum, err := strconv.Atoi(os.Getenv(dbName + "_NUM"))
 	if err != nil {
 		return nil, err
 	}
 
+	fmt.Println(dbName)
 	dbPort, err := strconv.Atoi(os.Getenv(dbName + "_PORT"))
+	if err != nil {
+		return nil, err
+	}
 
 	redisClient := RedisWrapper{
 		BaseWrapper: BaseWrapper{			
@@ -91,6 +107,8 @@ func (p *DBPool) NewRedisWrapper(dbName string) (*RedisWrapper, error) {
 		},
 		DbNum: dbNum,
 	}
+
+	p.Pool[dbName] = &redisClient
 
 	return &redisClient, err
 }
@@ -108,18 +126,72 @@ func (r *RedisWrapper) Connect(ctx context.Context) error {
 		fmt.Fprintln(os.Stderr, err)
 		log.Fatalf("Redis connection failed %v", err)
 	}
-	fmt.Println("Connected to" + r.Container)
+	fmt.Println("Connected to " + r.Container)
 
 	r.DB = rdb
 	return nil
 }
 
-func (r *RedisWrapper) getData() {
+func (r *RedisWrapper) GetData(query string, values []any) (any, error) {
+	val := r.DB.Get(context.Background(), values[0].(string))
 
+	returnedData, err := val.Result()
+	if err != nil {
+		return nil, err
+	}
+
+	return returnedData, nil
 }
 
-func (r *RedisWrapper) setData() {
+func (r *RedisWrapper) SetData(query string, values []any, duration *time.Duration) error {
+	ret := r.DB.Set(context.Background(), values[0].(string), values[1], *duration)
 
+	if ret.Err() != nil {
+		return ret.Err()
+	}
+
+	return nil
+}
+
+func (r *RedisWrapper) GetKey(id string) (any, error) {
+	val := r.DB.Get(context.Background(), id)
+	fmt.Println(val)
+
+	returnedData, err := func () (*memguard.Enclave, error) {
+		valRaw, err := val.Result()
+		if err != nil {
+			return nil, err
+		}
+
+		fmt.Println(valRaw)
+		valKey, err := base64.StdEncoding.DecodeString(valRaw)
+
+		fmt.Println(valKey)
+		return memguard.NewEnclave(valKey), err
+	}()
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println(reflect.TypeOf(returnedData))
+
+	return returnedData, nil
+}
+
+func (r *RedisWrapper) SetKey(id string, key any, duration *time.Duration) error {
+	keyLocked, err := key.(*memguard.Enclave).Open()
+	if err != nil {
+		return err
+	}
+
+	ret := r.DB.Set(context.Background(), id, keyLocked.Bytes(), *duration)
+	keyLocked.Destroy()
+
+	if ret.Err() != nil {
+		return ret.Err()
+	}
+
+	return nil
 }
 
 
@@ -135,9 +207,14 @@ type MySQLWrapper struct {
 	User string
 }
 
-func (p DBPool) NewSQLWrapper(dbName string, connectff bool) *MySQLWrapper{
-	if p.pool[dbName] != nil {
-		return p.pool[dbName].(*MySQLWrapper)
+func (p DBPool) NewSQLWrapper(dbName string) (*MySQLWrapper, error){
+	if p.Pool[dbName] != nil {
+		err := p.Pool[dbName].Connect(context.Background())
+		if err != nil {
+			return nil, err
+		}
+
+		return p.Pool[dbName].(*MySQLWrapper), nil
 	}
 
 	sqlWrapper := MySQLWrapper{
@@ -153,17 +230,13 @@ func (p DBPool) NewSQLWrapper(dbName string, connectff bool) *MySQLWrapper{
 		User: os.Getenv(dbName  + "_USER"),
 	}
 
-	if connectff {
-		sqlWrapper.Connect(context.Background())
-	}
+	fmt.Println(dbName)
+	p.Pool[dbName] = &sqlWrapper
 
-	p.pool[dbName] = sqlWrapper
-
-	return &sqlWrapper
+	return &sqlWrapper, nil
 }
 
 func (sr *MySQLWrapper) Connect(ctx context.Context) error {
-	fmt.Println("test")
 	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", sr.User, sr.Password, sr.Container, sr.Port, sr.DBname))
 	if err != nil {
 		return err
@@ -185,17 +258,42 @@ func (sr *MySQLWrapper) Close() error {
 	return nil
 }
 
-// expectedDts: map[string]any
-func (sr *MySQLWrapper) GetData(query string) map[int]map[string]any {
-	rows, err := sr.DB.Query(query)
+// key is stored encrypted in its raw form (no b64 encoding etc.)
+func (sr *MySQLWrapper) GetKey(id string) (any, error) {
+	var returnedValue any
+	err := sr.DB.QueryRow("SELECT k_val FROM kstore WHERE id = ?", id).Scan(&returnedValue)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
+	}
+
+
+	return returnedValue, nil
+}
+
+func (sr *MySQLWrapper) SetKey(id string, key any, d *time.Duration) error {
+	_, err := sr.DB.Exec("INSERT INTO kstore (id, k_val) VALUES (?, ?)", key.(string))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// expectedDts: map[string]any
+func (sr *MySQLWrapper) GetData(query string, values []any) (any, error) {
+	rows, err := sr.DB.Query(query, values...)
+	if err != nil {
+		return nil, err
 	}
 	defer rows.Close()
 
+	if !rows.Next() {
+		return nil, errors.New("failed to get data (no match in db)")
+	}
+
 	columns, err := rows.Columns()
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
 	returnedMap := make(map[int]map[string]any)
@@ -204,7 +302,7 @@ func (sr *MySQLWrapper) GetData(query string) map[int]map[string]any {
 
 	for rows.Next() {
 		if err := rows.Scan(uncastedRowValues...); err != nil {
-			log.Fatal(err)
+			return nil, err
 		}
 
 		for i, v := range columns {
@@ -214,17 +312,17 @@ func (sr *MySQLWrapper) GetData(query string) map[int]map[string]any {
 		returnedMap[uncastedRowValues[0].(int)] = emptyMap 
 	}
 
-	return returnedMap
+	return returnedMap, nil
 }
 
-func (sr *MySQLWrapper) SetData(query string, values []any) {
-	fmt.Println(sr.DB)
-
+func (sr *MySQLWrapper) SetData(query string, values []any, n *time.Duration) error {
 	result, err := sr.DB.Exec(query, values...)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	// audit relevant
 	fmt.Println(result)
+
+	return nil
 }
