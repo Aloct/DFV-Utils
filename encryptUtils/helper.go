1b6f5e98323099eae383b5fcebef9a779e90a5fd
Aloct <aloct.itw@gmail.com>
2025-02-23 14:36:27 +0100

    fixed gatewaykeys public key insertion

--- encryptUtils/helper.go
+++ encryptUtils/helper.go
@@ -1,7 +1,33 @@
 package encryptUtils
 
+import (
+	"encoding/hex"
+	"reflect"
+)
+
 func toZero(byteSlice []byte) {
 	for i := range byteSlice {
 		byteSlice[i] = 0
 	}
+}
+
+func KeyToString(key []byte) string {
+	return hex.EncodeToString(key)
+}
+
+func StringToKey(keyRaw string) ([]byte, error) {
+	var keyString string
+
+	if (reflect.TypeOf(keyRaw).Kind() == reflect.String) {
+		keyString = keyRaw
+	} else if (reflect.TypeOf(keyRaw).Kind() == reflect.Slice) { 
+		keyString = string(keyRaw)
+	}
+
+	decodedKey, err := hex.DecodeString(keyString)
+	if err != nil {
+		return nil, err
+	}
+
+	return decodedKey, err
 }
--- encryptUtils/keys.go
+++ encryptUtils/keys.go
@@ -18,16 +18,16 @@ import (
 )
 
 type kekRefs struct {
-	KEKID string
-	Manager string
-	KEKDB string
-	KEKCache string
-	Cached time.Time
+	KEKID         string
+	Manager       string
+	KEKDB         string
+	KEKCache      string
+	Cached        time.Time
 	CacheDuration time.Duration
 }
 
 type DEKCombKEK struct {
-	Dek []byte
+	Dek   []byte
 	KekDB kekRefs
 }
 
@@ -98,11 +98,11 @@ func CreateDEKCombKEK(dek *memguard.Enclave, kekDB string, kekCache string, cach
 	return &DEKCombKEK{
 		Dek: dekEncrypted,
 		KekDB: kekRefs{
-			KEKID: resData["data"].(string),
-			Manager: fmt.Sprintf("%s:%d", managerC, managerP),
-			KEKDB: kekDB,
-			KEKCache: kekCache,
-			Cached: time.Now().Add(-cacheDuration),
+			KEKID:         resData["data"].(string),
+			Manager:       fmt.Sprintf("%s:%d", managerC, managerP),
+			KEKDB:         kekDB,
+			KEKCache:      kekCache,
+			Cached:        time.Now().Add(-cacheDuration),
 			CacheDuration: cacheDuration,
 		},
 	}, nil
@@ -129,7 +129,7 @@ func (dc *DEKCombKEK) decryptKEK(dbF keyFetcher, cacheF keyFetcher) (*memguard.E
 	// check if KEK is cached
 	var kek *memguard.Enclave
 
-	if (time.Since(dc.KekDB.Cached) < dc.KekDB.CacheDuration) {
+	if time.Since(dc.KekDB.Cached) < dc.KekDB.CacheDuration {
 		kekRaw, err := cacheF.GetKey(dc.KekDB.KEKID)
 		if err != nil {
 			return nil, err
@@ -147,24 +147,24 @@ func (dc *DEKCombKEK) decryptKEK(dbF keyFetcher, cacheF keyFetcher) (*memguard.E
 		if err != nil {
 			return nil, err
 		}
-	
+
 		req, err := http.NewRequest("POST", fmt.Sprintf("https://%s/decryptKEK", dc.KekDB.Manager), bytes.NewBuffer(jsonData))
 		if err != nil {
 			return nil, err
 		}
-	
+
 		req.Header.Add("Content-Type", "application/json")
-	
+
 		client := &http.Client{}
 		res, err := client.Do(req)
 		if err != nil {
 			return nil, err
 		}
-	
+
 		if res.StatusCode != http.StatusFound {
 			return nil, errors.New("failed to decrypt KEK in auth manager")
 		}
-	
+
 		body, err := io.ReadAll(res.Body)
 		if err != nil {
 			return nil, err
@@ -182,7 +182,7 @@ func (dc *DEKCombKEK) decryptKEK(dbF keyFetcher, cacheF keyFetcher) (*memguard.E
 			b64Raw := splitted[0]
 
 			enclave := memguard.NewEnclave([]byte(b64Raw))
-	
+
 			// cache key to increase decryption speed
 			cacheF.SetKey(dc.KekDB.KEKID, enclave, &dc.KekDB.CacheDuration)
 			dc.KekDB.Cached = time.Now()
@@ -191,7 +191,7 @@ func (dc *DEKCombKEK) decryptKEK(dbF keyFetcher, cacheF keyFetcher) (*memguard.E
 			if err != nil {
 				return nil, err
 			}
-	
+
 			return memguard.NewEnclave(b64Decoded), nil
 		}()
 
@@ -206,7 +206,7 @@ func (dc *DEKCombKEK) decryptKEK(dbF keyFetcher, cacheF keyFetcher) (*memguard.E
 func CreateAESKey(keySize int) (*memguard.Enclave, error) {
 	if keySize != 16 && keySize != 24 && keySize != 32 && keySize != 64 {
 		return nil, errors.New("no valid key byte slice provided")
-	} 
+	}
 
 	keyRaw := make([]byte, keySize)
 	if _, err := rand.Read(keyRaw); err != nil {
@@ -225,7 +225,7 @@ func CreateECCKey(publicDBStore keyFetcher) (*memguard.Enclave, error) {
 		return nil, err
 	}
 
-	publicDBStore.SetData("INSERT INTO kstore (id, k_val) VALUES (?, ?)", []any{"1", hex.EncodeToString(public)}, nil)
+	publicDBStore.SetData("INSERT INTO kstore (id, k_val, created_at, last_swap) VALUES (?, ?, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP) ON DUPLICATE KEY UPDATE k_val = VALUES(k_val), created_at = CURRENT_TIMESTAMP, last_swap = CURRENT_TIMESTAMP", []any{"1", hex.EncodeToString(public)}, nil)
 
 	return memguard.NewEnclave(private), nil
 }
